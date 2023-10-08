8c9e3871f8f33c912bba446225199b66cefe86cc
Evarmedia <mishakmanuel@gmail.com>
2023-10-08 20:37:42 +0000

    task7+test

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,4 +5,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
     x2 = tuple_b[0] if len(tuple_b) >= 1 else 0 # Get the first element of tuple_b or use 0 if it is missing
     y2 = tuple_b[1] if len(tuple_b) >= 2 else 0 # Get the second element of tuple_b or use 0 if it is missing
     return (x1 + x2, y1 + y2) # Return a tuple with the sums of corresponding elements
-
