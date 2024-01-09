513ef87718dbd4ae957fd3fa6e256a6acf01ad1e
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 17:07:19 +0100

    tsk#3-semistandard style fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,19 +3,19 @@
 // a class that defines a rectangle(with an instance that prints x)
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (parseInt(w) > 0 && parseInt(h) > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
-      for(let i = 0; i < this.height; i++) {
-        let theRow = "";
-        for (let k = 0; k < this.width; k++){
-          theRow += "X";
+      for (let i = 0; i < this.height; i++) {
+        let theRow = '';
+        for (let k = 0; k < this.width; k++) {
+          theRow += 'X';
         }
         console.log(theRow);
       }
