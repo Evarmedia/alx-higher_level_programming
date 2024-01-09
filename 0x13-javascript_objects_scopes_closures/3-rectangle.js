0aac93e34e07dd5a9ba04846ac0daf07c2057b05
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 16:56:00 +0100

    tsk#3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+// a class that defines a rectangle(with an instance that prints x)
+
+class Rectangle {
+  constructor(w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width && this.height) {
+      for(let i = 0; i < this.height; i++) {
+        let theRow = "";
+        for (let k = 0; k < this.width; k++){
+          theRow += "X";
+        }
+        console.log(theRow);
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;
