87fa7ccb34f9d274d4a557f92d6f56c3b04d922b
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 19:32:30 +0100

    tsk#13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// a script that concats two files
+const fs = require('fs');
+
+const sourceFilePath1 = process.argv[2];
+const sourceFilePath2 = process.argv[3];
+const destinationFilePath = process.argv[4];
+
+const readStream1 = fs.createReadStream(sourceFilePath1);
+const readStream2 = fs.createReadStream(sourceFilePath2);
+const writeStream = fs.createWriteStream(destinationFilePath, { flags: 'a' });
+
+readStream1.pipe(writeStream, { end: false });
+
+readStream1.on('end', () => {
+  readStream2.pipe(writeStream);
+});
+
+readStream2.on('end', () => {
+  console.log('Files concatenated successfully!');
+});
