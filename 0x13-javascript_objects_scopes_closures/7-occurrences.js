9e16459b14ff82feecb12eed4e01a0abfcff18dc
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 17:57:04 +0100

    tsk#7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// a function that returns the number of occurences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let ind = 0; ind < list.length; ind++) {
+    if (list[ind] === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+}
