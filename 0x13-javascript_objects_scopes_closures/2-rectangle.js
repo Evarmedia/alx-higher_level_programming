2fb6795c3530fa595f454446838a7d3d551f4999
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 16:29:24 +0100

    tsk#2-style

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 // a class that defines a rectangle:)
+
 class Rectangle {
-	constructor(w,h) {
-		if ( parseInt(w) > 0 && parseInt(h) > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor(w,h) {
+    if ( parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
