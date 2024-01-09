4d73b9f88e91a284d8a23546b7af4948b3584720
Evarmedia <mishakmanuel@gmail.com>
2024-01-09 19:41:53 +0100

    tsk10-fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,7 +3,8 @@
 // function that converts a number from base 10 to another base passed as argument
 
 exports.converter = function (base) {
-  return function conversion (number) {
-    return (number === 0) ? '' : conversion(Math.floor(number / base)) + (number % base);
+  return function convertToBase(number) {
+    return number.toString(base);
   };
 };
+
