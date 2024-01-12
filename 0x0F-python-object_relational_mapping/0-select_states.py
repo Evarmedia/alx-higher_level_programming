c6fa65938e56131a55a52e50edaeb485a5d00b23
Evarmedia <mishakmanuel@gmail.com>
2024-01-12 10:03:08 +0100

    tsk#0---

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-
-""" 
-a script that lists all states from database
 """
+Lists all `states` from the database `hbtn_0e_0_usa`
+"""
+
 import MySQLdb
 from sys import argv, stderr
 
 
 def listStates():
-    """function to list the states in a given database"""
+    """lists all the states in the given database"""
 
-    err_message = "usage: {} <username> <pasword> <db_name>\n".format(argv[0])
+    err_msg = "usage: {} <username> <password> <db_name>\n".format(argv[0])
     if len(argv) != 4:
-        stderr.write(err_message)
+        stderr.write(err_msg)
         return
 
     username, password, db_name = argv[1], argv[2], argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
-	user=username,
-	passwd=password,
-	db=db_name,
-	charset="utf8",
-	port=3306
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8",
+        port=3306
     )
 
     cur = db.cursor()
