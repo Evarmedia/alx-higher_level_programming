cbfab14ab80f049087da8b69def93cce867ee795
Evarmedia <mishakmanuel@gmail.com>
2024-01-12 09:25:06 +0100

    t#0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" 
+a script that lists all states from database
+"""
+import MySQLdb
+from sys import argv, stderr
+
+def listStates():
+    """function to list the states in a given database"""
+    err_message = "usage: {} <username> <pasword> <db_name>\n".format(argv[0])
+    if len(argv) != 4:
+        stderr.write(err_message)
+        return
+    username, password, db_name = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(
+        host = "localhost",
+	user = username,
+	passwd = password,
+	db = db_name,
+	charset = "utf8",
+	port = 3306
+    )
+
+    cur = db.cursor()
+    cur.exexute("SELECT * FROM states ORDER BY id ASC")
+    result = cur.fetchall()
+    
+    for row in results:
+        print(row)
+
+    db.close()
+if __name__ == "__main__":
+    listStates()
