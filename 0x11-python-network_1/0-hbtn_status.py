f52b1baee299e7387d1ee6c98afdf2845daaeae6
Evarmedia <mishakmanuel@gmail.com>
2024-01-26 12:54:53 +0100

    pyNetwork#0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,24 +2,29 @@
 """This module contains the code to data fetch `https://alx-intranet.hbtn.io/status`"""
 
 from urllib.request import urlopen, Request
-from urllib.error import URLError, HTTPError
+from urllib.error import URLError
 
-def fetch_body():
-    """Fetches the site and the body"""
-    target_url = "https://alx-intranetbtn.io/status"
-    request = Request(target_url)
+
+def fetch_body(site_url):
+    """Fetches the data and displays the body"""
+
+    reqst = Request(site_url)
 
     try:
-        response = urlopen(request)
-        response_body = response.read()
+        with urlopen(reqst) as response:
+            response_body = response.read()
+    except URLError as err:
+        if hasattr(err, 'reason'):
+            print("Reason: ", err.reason)
+        elif hasattr(err, 'code'):
+            print("Error code: ", err.code)
+    else:
         print("Body response:")
         print("\t- type: {}".format(type(response_body)))
         print("\t- content: {}".format(response_body))
         print("\t- utf8 content: {}".format(response_body.decode("utf-8")))
-    except HTTPError as error:
-        print("Error code: ", error.code)
-    except URLError as error:
-        print("Reason: ", error.reason)
+
 
 if __name__ == "__main__":
-    fetch_body()
+    fetch_body("https://alx-intranet.hbtn.io/status")
+    
