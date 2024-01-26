ba65cece1a68a1d49bf35347b420799a1b4870f7
Evarmedia <mishakmanuel@gmail.com>
2024-01-26 12:34:02 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""This module contains code to fetch data from `https://alx-intranet.hbtn.io/status`"""
+"""This module contains the code to data fetch `https://alx-intranet.hbtn.io/status`"""
+
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+url = 'https://alx-inet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-print('- Body response:')
-print('\t- type:', type(body))
-print('\t- content:', body)
-print('\t- utf8 content:', body.decode('utf-8'))
+    print('- Body response:')
+    print('\t- type:', type(body))
+    print('\t- content:', body)
+    print('\t- utf8 content:', body.decode('utf-8'))
+
+except Exception as e:
+    print('An error occurred:', e)
