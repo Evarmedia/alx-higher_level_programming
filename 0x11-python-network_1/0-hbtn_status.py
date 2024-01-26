16e66510d34e0a11af6dd417496f2085da3adbec
Evarmedia <mishakmanuel@gmail.com>
2024-01-26 12:47:20 +0100

    fff

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 """This module contains the code to data fetch `https://alx-intranet.hbtn.io/status`"""
 
-import urllib.request
+from urllib.request import urlopen, Request
+from urllib.error import URLError
 
-url = 'https://alx-inet.hbtn.io/status'
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+def fetch_body(site_url):
+    """Fetches the data and displays the body"""
 
-    print('Body response:')
-    print('\t- type:', type(body))
-    print('\t- content:', body)
-    print('\t- utf8 content:', body.decode('utf-8'))
+    reqst = Request(site_url)
 
-except Exception as e:
-    print('An error occurred:', e)
+    try:
+        with urlopen(reqst) as response:
+            response_body = response.read()
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print("Reason: ", e.reason)
+        elif hasattr(e, 'code'):
+            print("Error code: ", e.code)
+    else:
+        print("Body response:")
+        print("\t- type: {}".format(type(response_body)))
+        print("\t- content: {}".format(response_body))
+        print("\t- utf8 content: {}".format(response_body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetch_body("https://alx-intranet.hbtn.io/status")
+    
