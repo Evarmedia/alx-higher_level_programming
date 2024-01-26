f589032078679624435466a8010e35b52bbe7682
Evarmedia <mishakmanuel@gmail.com>
2024-01-26 12:35:52 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ try:
     with urllib.request.urlopen(url) as response:
         body = response.read()
 
-    print('- Body response:')
+    print('Body response:')
     print('\t- type:', type(body))
     print('\t- content:', body)
     print('\t- utf8 content:', body.decode('utf-8'))
