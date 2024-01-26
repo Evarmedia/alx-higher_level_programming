0376d16ae8f8d4976ef9ce6f366a562c95e63d10
Evarmedia <mishakmanuel@gmail.com>
2024-01-26 12:23:56 +0100

    task#0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 """This module contains code to fetch data from `https://alx-intranet.hbtn.io/status`"""
 import urllib.request
-import sys
 
-def fetch_x_request_id(url):
-    """Send a request to the URL and display the value of the X-Request-Id variable"""
-    req = urllib.request.Request(url)
-    
-    try:
-        with urllib.request.urlopen(req) as response:
-            x_request_id = response.getheader('X-Request-Id')
-            print("Value of X-Request-Id variable:", x_request_id)
-    except urllib.error.URLError as e:
-        if hasattr(e, 'reason'):
-            print("Failed to reach the server. Reason:", e.reason)
-        elif hasattr(e, 'code'):
-            print("The server couldn't fulfill the request. Error code:", e.code)
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    fetch_x_request_id(url)
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print('- Body response:')
+print('\t- type:', type(body))
+print('\t- content:', body)
+print('\t- utf8 content:', body.decode('utf-8'))
