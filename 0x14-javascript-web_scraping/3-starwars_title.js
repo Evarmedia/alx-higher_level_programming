299b5eee0f462167c535c43f2f9d473232fb778d
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:07:35 +0100

    #3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  } else {
+    console.error('An error occurred while fetching the movie title:', error);
+  }
+});
