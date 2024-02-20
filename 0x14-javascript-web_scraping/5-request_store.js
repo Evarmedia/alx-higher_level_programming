4dd5ddaec046d46c3bab082778578680c05cfb84
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:29:11 +0100

    #ts5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,4 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error('An error occurred while writing to the file:', err);
-      } else {
-        console.log('The file has been saved!');
-      }
-    });
-  } else {
-    console.error('An error occurred while fetching the webpage:', error);
-  }
-});
+request(process.argv[2], (error, response, body) => { fs.writeFile(process.argv[3], body, 'utf-8', () => {}); });
