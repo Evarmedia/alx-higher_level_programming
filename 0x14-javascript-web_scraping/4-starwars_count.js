e129eda46d2996155ddbf9646c8af77e7aaabc09
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:11:41 +0100

    #4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    const count = films.reduce((acc, film) => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    console.log(count);
+  } else {
+    console.error('An error occurred while fetching the movie count:', error);
+  }
+});
