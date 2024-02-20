186d28ad7f93a3bdde1a9d51c1d303841ab14e9d
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:36:20 +0100

    #ts6-adv

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
-request(process.argv[2], (error, response, body) => { fs.writeFile(process.argv[3], body, 'utf-8', () => {}); });
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  err ?
+    console.log(err) :
+    response.statusCode === 200 ?
+      (() => {
+        const completed = {};
+        const tasks = JSON.parse(body);
+        for (const i in tasks) {
+          const task = tasks[i];
+          task.completed === true ?
+            completed[task.userId] === undefined ?
+              completed[task.userId] = 1 :
+              completed[task.userId]++ :
+            null;
+        }
+        console.log(completed);
+      })() :
+      console.log('An error occured. Status code: ' + response.statusCode);
+});
