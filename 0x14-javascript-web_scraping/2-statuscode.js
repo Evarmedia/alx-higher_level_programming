39050079e017a7b6adf3f64f882b6b8581fbe732
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:04:45 +0100

    #task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response) => {
-  if (!error) {
     console.log('code:' response.statusCode);
-  } else {
-    console.error(error);
-  }
 });
