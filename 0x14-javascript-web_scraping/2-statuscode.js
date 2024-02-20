15e20ee6860a790c2f7ef4f48ae05b99df926d9a
Evarmedia <mishakmanuel@gmail.com>
2024-02-20 17:06:04 +0100

    #tssk2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-
-const url = process.argv[2];
-
-request(url, (error, response) => {
-    console.log('code:' response.statusCode);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
