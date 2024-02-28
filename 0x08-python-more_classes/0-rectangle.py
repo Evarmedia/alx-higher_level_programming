2f9d03da7041a0a0f063d620b392bfb8fc37e8dc
Evarmedia <mishakmanuel@gmail.com>
2024-02-28 15:00:35 +0100

    jquery

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-This is the ``0-rectangle`` module
-
-It contains the empty class ``Rectangle``
-"""
-
 
 class Rectangle:
     """Empty class Rectangle that defines (or not) a rectangle.
